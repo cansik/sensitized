795e5c14d1e36f7c634c034810d48a09402e8520
Florian Bruggisser <florian@nexpose.ch>
2018-03-15 16:28:40 +0100

    added fps tracking

--- src/main/kotlin/ch/bildspur/yolo/Sketch.kt
+++ src/main/kotlin/ch/bildspur/yolo/Sketch.kt
@@ -2,6 +2,7 @@ package ch.bildspur.yolo
 
 import ch.bildspur.yolo.io.ImageSource
 import ch.bildspur.yolo.io.SingleImageSource
+import ch.bildspur.yolo.util.ExponentialMovingAverage
 import ch.bildspur.yolo.vision.ImageDetector
 import ch.bildspur.yolo.vision.YoloDetector
 import ch.bildspur.yolo.vision.format
@@ -15,6 +16,8 @@ class Sketch : PApplet() {
 
     var minConfidence = 0.8
 
+    var fpsAverage = ExponentialMovingAverage(0.1)
+
     override fun settings() {
         size(512, 512, FX2D)
     }
@@ -22,8 +25,6 @@ class Sketch : PApplet() {
     override fun setup() {
         source.setup(this)
         detector.setup(this)
-
-        noLoop()
     }
 
     override fun draw() {
@@ -33,7 +34,6 @@ class Sketch : PApplet() {
         val result = detector.detect(image.toMat())
         image(source.readImage(), 0f, 0f)
 
-
         // draw results
         result.detections.filter { it.confidence > minConfidence }
                 .forEach{
@@ -42,10 +42,14 @@ class Sketch : PApplet() {
                     strokeWeight(2f)
                     rect(it.x.toFloat(), it.y.toFloat(), it.width.toFloat() * image.width, it.height.toFloat() * image.height)
 
-                    textSize(18f)
+                    textSize(14f)
                     fill(0f, 255f, 0f)
                     text("${it.name.toUpperCase()} (${it.confidence.format(2)})", it.x.toFloat(), it.y.toFloat() - 10)
                 }
+
+        // show fps
+        fpsAverage += frameRate.toDouble()
+        surface.setTitle("YOLO | FPS: ${frameRate.format(2)}\tAVG: ${fpsAverage.average.format(2)}")
     }
 
     override fun stop() {
